117c484aaf5916a418b8b8f88567f70690c68517
taise <taise515@gmail.com>
2013-11-03 15:25:30 +0900

    Add RequestController index method & Test

---
+++ test/controllers/RequestControllerTest.java
@@ -0,0 +1,28 @@
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.data.DynamicForm;
+import play.data.validation.ValidationError;
+import play.data.validation.Constraints.RequiredValidator;
+import play.i18n.Lang;
+import play.libs.F;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+public class RequestControllerTest {
+
+  @Test
+  public void callIndex() {
+    Result result = callAction(
+        controllers.routes.ref.Application.index()
+        );
+
+    assertThat(status(result)).isEqualTo(OK);
+    assertThat(contentType(result)).isEqualTo("text/html");
+    assertThat(charset(result)).isEqualTo("utf-8");
+    //assertThat(contentAsString(result)).contains("Request.index");
+  }
+}
