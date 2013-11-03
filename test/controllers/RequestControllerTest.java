0a200b7b793ecb5e072ad606cf7986c0b48fc3d7
taise <taise515@gmail.com>
2013-11-03 15:54:04 +0900

    Refactoring: RequestControllerTest

--- test/controllers/RequestControllerTest.java
+++ test/controllers/RequestControllerTest.java
@@ -13,11 +13,7 @@ public class RequestControllerTest {
     Result result = callAction(
         controllers.routes.ref.RequestController.index()
         );
-
-    assertThat(status(result)).isEqualTo(OK);
-    assertThat(contentType(result)).isEqualTo("text/html");
-    assertThat(charset(result)).isEqualTo("utf-8");
-    assertThat(contentAsString(result)).contains("Request.index");
+    checkResult(result, "Request.index");
   }
 
   @Test
@@ -25,10 +21,13 @@ public class RequestControllerTest {
     Result result = callAction(
         controllers.routes.ref.RequestController.newPage()
         );
+    checkResult(result, "Request.new");
+  }
 
+  public void checkResult(Result result, String message) {
     assertThat(status(result)).isEqualTo(OK);
     assertThat(contentType(result)).isEqualTo("text/html");
     assertThat(charset(result)).isEqualTo("utf-8");
-    assertThat(contentAsString(result)).contains("Request.new");
+    assertThat(contentAsString(result)).contains(message);
   }
 }
