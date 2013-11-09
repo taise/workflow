2ddebd3aae7d97adbc1d4ae4b85c427386b9fbcf
taise <taise515@gmail.com>
2013-11-09 12:38:41 +0900

    Add User model & test

---
+++ app/models/User.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.*;
+import play.db.ebean.*;
+
+@Entity
+public class User extends Model {
+
+  @Id
+  public Long id;
+  public String userId;
+  public String name;
+  public String company;
+  public String division;
+  public String post;
+
+  public Date createdAt;
+  public Date updatedAt;
+
+  public User(String userId, String name,
+              String company, String division, String post) {
+    this.userId = userId;
+    this.name = name;
+    this.company = company;
+    this.division = division;
+    this.post = post;
+
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
---
+++ test/models/UserTest.java
@@ -0,0 +1,53 @@
+package models;
+
+import models.User;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+
+import java.util.*;
+import play.libs.Yaml;
+
+import static play.test.Helpers.*;
+
+public class UserTest {
+  @Before
+  public void setUp() {
+    start(fakeApplication(inMemoryDatabase()));
+  }
+  
+  @Test
+  public void constructor() {
+    Map<String,String> params = defaultParams();
+
+    User user = new User(
+        params.get("userId"),
+        params.get("name"),
+        params.get("company"),
+        params.get("division"),
+        params.get("post")
+        );
+  }
+
+  public Map defaultParams() {
+    Map<String,String> params = new HashMap<String,String>();
+    params.put("userId","000001");
+    params.put("name","Bob");
+    params.put("company","Apple");
+    params.put("division","Development");
+    params.put("post","boss");
+    return params;
+  }
+
+  public void assertUserModel(User user, Map<String,String> params) {
+    assertEquals(params.get("userId"), user.userId);
+    assertEquals(params.get("name"), user.name);
+    assertEquals(params.get("company"), user.company);
+    assertEquals(params.get("division"), user.division);
+    assertEquals(params.get("post"), user.post);
+    assertThat(new Date(), greaterThanOrEqualTo(user.createdAt));
+    assertThat(new Date(), greaterThanOrEqualTo(user.updatedAt));
+  }
+}
